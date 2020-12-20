e6fed582587a6882d34e724c7f0c69052d47d4ab
Carmen <carmenschmidt6@gmx.de>
2020-12-20 16:15:44 +0100

    updated to new amino variant

--- scripts/aminos.py
+++ scripts/aminos.py
@@ -4,6 +4,7 @@ import excel
 import os
 import datetime
 import pandas as pd
+import re
 from shutil import copyfile
 
 #setup logger for console and file
@@ -31,8 +32,8 @@ def read_config(config_file = 'config.json', create_new_file=False):
         data['export_directory'] = '../analysed/'
         data['file_extension_raw_data'] = '_Rohdaten.xlsx'
         data['file_extension_analysis'] = '_Analyse.xlsx'
-        data['ignore_samples'] = ['SIGMA200', 'SIGMA500', 'Phe200', 'Phe1000']
-        data['control_name_prefix'] = 'Ko'
+        data['ignore_calibration'] = "[KkCc]al\s?\d"
+        data['control_name'] = '(([CckK][oO])|([qQ][cC]))\\s?[I\\d]'
         data['control_ring_samples'] = [61, 62, 31, 32]
         data['max_normal_aminos'] = 21
         data['control_reference_file_path'] = './reference/kontrollwerte.csv'
@@ -91,22 +92,20 @@ def preparation(cfg, raw_data_file):
     copyfile(raw_data_file, os.path.join(export_dir, raw_copy_filename))
     
     return export_dir, excel_sheet
-    
+
 def filter_raw_data(cfg, data):
-    _logger.info("ignore samples: " + ', '.join(cfg['ignore_samples']))
+    _logger.info(f"remove calibration from data: {cfg['ignore_calibration']}")
     column_name = cfg['columns']['sample_name']
-    ignore = cfg['ignore_samples']
-    control_name = cfg['control_name_prefix']
-    data = data[data[column_name].isin(ignore) == False] # remove all unused samples from matrix
-    controls_idx = data[column_name].str.contains(control_name, na=False) # get idx with controls
-    
+    ignore_pattern = cfg['ignore_calibration']
+    control_name_pattern = cfg['control_name']
+    data = data.drop(data[data[column_name].str.match(ignore_pattern) == True].index) # remove all unused calibration
+    controls_idx = (data[column_name].str.match(control_name_pattern) == True)
     _logger.info("split into controls and patient data")
     controls = data[controls_idx]                # get controls from matrix
     data = data[controls_idx == False]           # remove controls from data matrix
-    # set invalid values to None
+    _logger.info(f"set invalid values 'No Peak' to None")
     controls = controls.replace("No Peak", None)
     data = data.replace("No Peak", None)
-    # end set
     _logger.info("sort data")
     controls[column_name] = controls[column_name].astype(str)
     controls.sort_values(column_name, axis=0, ascending=True, inplace=True) # sort ascending
@@ -117,89 +116,115 @@ def filter_raw_data(cfg, data):
 def check_controls(cfg, data):
     controls = data['controls']
     control_reference = data['control_reference']
-    ret = pd.DataFrame().reindex_like(controls)
-    ret[ret.isnull()] = 'NONE'
-    
-    str_ring_samples = ', '.join(str(s) for s in cfg['control_ring_samples'])
-    _logger.info(F"check for following controls: {str_ring_samples}")
+    results = []
     
     for idx_row, row_data in controls.iterrows():
         # search in the controls for control ring samples
         control_name = row_data[cfg['columns']['sample_name']]
-        matches = [num for num in cfg['control_ring_samples'] if str(num) in control_name]
-        # check if a match exist
-        if not len(matches):
-            _logger.error('control can not be found in settings "control_ring_samples"')
+        if 'I' in control_name:
+            raw_num = control_name.count('I')  # convert III integer
         else:
-            match = matches[0]
-            _logger.info(F"processing control {match} for {control_name}")
-            
-            # get the limits from the reference frame
-            matched_control = control_reference[control_reference['controls'] == match]
-            control_min = matched_control[matched_control['limits'] == 'min']
-            control_max = matched_control[matched_control['limits'] == 'max']
-            
-            for col in control_reference.columns.values:
-                val_min = control_min[col].item() 
-                val_max = control_max[col].item()
-                
-                col_names = controls.columns[controls.columns.str.contains(pat = col)]
-                if (len(col_names)):
-                
-                    control_idx_bo = (controls[cfg['columns']['sample_name']] == control_name)
-                    
-                    for col_name in col_names:
-                        _logger.debug(F"{col} = {col_name}: {val_min} < {controls[col_name][control_idx_bo].to_string(index=False)} > {val_max}")
-                        #print(F"{controls[col_name]}  :  {val_min}")
-                        #print(controls[col_name])
-                        ret[col_name][control_idx_bo & (controls[col_name] < val_min)] = 'TOO_LOW'
-                        ret[col_name][control_idx_bo & (controls[col_name] > val_max)] = 'TOO_HIGH'
-                        ret[col_name][control_idx_bo & (controls[col_name] <= val_max) & (controls[col_name] >= val_min)] = 'NORMAL'
-                        
-    return ret
-
-def select_control(cfg, data):
-    dat = {}
-    controls = data['controls']
-    checked_controls = data['checked_controls']
-    best_control = [0, 0]
-    second_best_control = [0, 0]
-    #max_prios_score = 0
-    #best_control = 0
-    dat['data'] = {}
-    for ring in cfg['control_ring_samples']:
-        column_name = cfg['columns']['sample_name']
-        # split up the controls
-        mask = controls[column_name].str.contains(str(ring))
-        if any(mask):
-            ring_data = {} 
-            ring_data['data'] = controls[mask]
-            ring_data['checked'] = checked_controls[mask]
-            
-            counts = ring_data['checked'].apply(pd.value_counts).fillna(0)
-            ring_data['score'] = counts[(counts.index == 'NORMAL')]
-            dat['data'][str(ring)] = ring_data
-            
-            ring_data['prios'], ring_data['conflicts'] = switch_amino_columns(cfg, ring, ring_data['score'])
-            ring_data['prios_score'] = ring_data['prios'].sum(axis = 1, skipna = True).item()
-            _logger.debug(ring_data['prios_score'])
-            
-            if best_control[1] < ring_data['prios_score']:
-                second_best_control = best_control.copy()
-                best_control[1] = ring_data['prios_score'] 
-                best_control[0] = ring
-    
-    dat['best_control_score'] = best_control[1]
-    dat['best_control_name'] = best_control[0]
-    dat['second_best_control_score'] = second_best_control[1]
-    dat['second_best_control_name'] = second_best_control[0]
-    
-    if dat['best_control_score'] == dat['best_control_name']:
-        _logger.warnung("both controls does have the same score, took first")
-    _logger.info(F"1. control: {str(dat['best_control_name'])}, score: {str(dat['best_control_score'])}")
-    _logger.info(F"2. control: {str(dat['second_best_control_name'])}, score: {str(dat['second_best_control_score'])}")
+            raw_num = int(re.search(r'(\d)', control_name)[0])
+        _logger.info(F"Get reference control dataset {raw_num} for control {control_name}")
         
-    return dat
+        # get the limits from the reference frame
+        matched_control = control_reference[control_reference['controls'] == raw_num]
+        
+        meas_con = row_data[2:]
+        ref_min = matched_control.iloc[0, 2:]
+        ref_mean = matched_control.iloc[2, 2:]
+        ref_max = matched_control.iloc[2, 2:]
+        
+        # get the shape and initialize with NORMAL
+        res = meas_con.copy()
+        res.iloc[:] = 'NORMAL'
+        # mark as too high or too low
+        res[meas_con < ref_min] = 'TOO_LOW'
+        res[meas_con > ref_max] = 'TOO_HIGH'
+        
+        # get coarse score
+        too_high_count = res[res=='TOO_HIGH'].count()
+        too_low_count = res[res=='TOO_LOW'].count()
+        coarse_score = 20 - too_high_count - too_low_count
+        
+        # get fine score 
+        error = (meas_con - ref_mean).abs()
+        delta = (ref_mean - ref_min).abs()
+        fine_score = round((1 - error/delta).mean(), 3)
+        
+        results.append({'name': control_name, 'result': res, 'coarse_score': coarse_score, 'fine_score': fine_score, 'raw_data': row_data})
+        _logger.info(f'{control_name} score: {coarse_score}/{fine_score}')
+        
+    return results
+
+def select_control(data):
+    data['checked_controls'] = sorted(data['checked_controls'], key=lambda i: (i['coarse_score'], i['fine_score']), reverse=True)
+    best = data['checked_controls'][0]
+    _logger.info(F'best control: {best["name"]} with score {best["coarse_score"]}/{best["fine_score"]}')
+#    
+#    #    
+#    #    
+#    #    series = val['result']
+#    #    too_high_count = series[series=='TOO_HIGH'].count()
+#    #    too_low_count = series[series=='TOO_LOW'].count()
+#    #    val['score'] = 20 - too_high_count - too_low_count
+#        #print(key)
+#        #print(series[series=='NORMAL'].count())
+#        #print(series[series=='TOO_HIGH'].count())
+#        #print(series[series=='TOO_LOW'].count())
+#    #print(data['checked_controls'])    
+#        
+#    exit()
+#    
+#    
+#    dat = {}
+#    
+#    
+#    controls = data['controls']
+#    checked_controls = data['checked_controls']
+#    
+#    print(checked_controls)
+#    print(controls)
+#    exit()
+#    best_control = [0, 0]
+#    second_best_control = [0, 0]
+#    #max_prios_score = 0
+#    #best_control = 0
+#    dat['data'] = {}
+#    for ring in cfg['control_ring_samples']:
+#        column_name = cfg['columns']['sample_name']
+#        # split up the controls
+#        mask = controls[column_name].str.contains(str(ring))
+#        if any(mask):
+#            ring_data = {} 
+#            ring_data['data'] = controls[mask]
+#            ring_data['checked'] = checked_controls[mask]
+#            
+#            counts = ring_data['checked'].apply(pd.value_counts).fillna(0)
+#            ring_data['score'] = counts[(counts.index == 'NORMAL')]
+#            dat['data'][str(ring)] = ring_data
+#            
+#            ring_data['prios'], ring_data['conflicts'] = switch_amino_columns(cfg, ring, ring_data['score'])
+#            ring_data['prios_score'] = ring_data['prios'].sum(axis = 1, skipna = True).item()
+#            _logger.debug(ring_data['prios_score'])
+#            
+#            if best_control[1] < ring_data['prios_score']:
+#                second_best_control = best_control.copy()
+#                best_control[1] = ring_data['prios_score'] 
+#                best_control[0] = ring
+#    
+#    dat['best_control_score'] = best_control[1]
+#    dat['best_control_name'] = best_control[0]
+#    dat['second_best_control_score'] = second_best_control[1]
+#    dat['second_best_control_name'] = second_best_control[0]
+#    
+#    if dat['best_control_score'] == dat['best_control_name']:
+#        _logger.warnung("both controls does have the same score, took first")
+#    _logger.info(F"1. control: {str(dat['best_control_name'])}, score: {str(dat['best_control_score'])}")
+#    _logger.info(F"2. control: {str(dat['second_best_control_name'])}, score: {str(dat['second_best_control_score'])}")
+#    
+#    exit()    
+    return best['name']
 
 def switch_amino_columns(cfg, control, score):
     ret = pd.DataFrame().reindex_like(score)
@@ -235,9 +260,10 @@ def filter_patients_data(cfg, data):
     if cfg['prefer_control'] != 0:
         best_control = str(cfg['prefer_control'])
     else:
-        best_control = str(data['selected_control']['best_control_name'])
+        best_control = str(data['selected_control'])
     dat = data['selected_control']['data'][best_control]['prios']
-    
+    print(dat)
+    exit()
     idx_not_null = dat.isnull() == False
     idx_zero = dat == 0
     
@@ -246,7 +272,7 @@ def filter_patients_data(cfg, data):
     for idx, val in idx_not_null.T.iterrows(): 
         if val.item() == True:
             idx_valids.append(idx_not_null.columns.get_loc(idx))
-    
+    #print(idx_valids)
     idx_invalids = []
     for idx, val in idx_zero.T.iloc[2:].iterrows(): 
         if val.item() == True:
@@ -272,7 +298,8 @@ def filter_patients_data(cfg, data):
     control = data['selected_control']['data'][best_control]['data'].copy()
     control = control[patients.columns[idx_valids]]
     new_control = control.reindex(sorted_cols, axis=1)
-    
+    print(idx_invalids)
+    print(data['checked_controls'])
     return (new_patients, idx_invalids, new_control)
 
 def analyse(cfg):
@@ -292,8 +319,8 @@ def analyse(cfg):
     data['control_reference'] = read_reference_data(cfg['control_reference_file_path'])
     data['patients_reference'] = read_reference_data(cfg['patients_reference_file_path'])
     data['checked_controls'] = check_controls(cfg, data)
-    data['selected_control'] = select_control(cfg, data)
-    data['data_filtered'], data['idx_invalids'], data['control_filtered'] = filter_patients_data(cfg, data)
+    data['selected_control'] = select_control(data)
+    #data['data_filtered'], data['idx_invalids'], data['control_filtered'] = filter_patients_data(cfg, data)
     
     # temporaly write into file
     # with open('data.pickle', 'wb') as handle:
--- scripts/excel.py
+++ scripts/excel.py
@@ -1,11 +1,15 @@
 import xlsxwriter
 import pandas as pd
 import logging
+import os
+import datetime
+from win32com import client
+import win32api
+
 _logger = logging.getLogger("excel")
 
-def export(cfg, filename, data):    
+def export(cfg, filename, data):
     workbook = xlsxwriter.Workbook(filename)
-    
     _logger.info("write raw data to excel")
     write_raw_data(workbook, data, cfg)
         
@@ -14,11 +18,12 @@ def export(cfg, filename, data):
    
     _logger.info("write patients data")
     write_patients_data(workbook, data, cfg)
-    
-    _logger.info("write control data")
-    write_control_data(workbook, data, cfg)
-    
+   
     workbook.close()
+    
+    exceltopdf(filename)
+    command = f"start EXCEL.EXE {filename}"
+    os.system(command)
 
 def write_raw_data(workbook, data, cfg):
     ws_raw_data = workbook.add_worksheet('Rohdaten')
@@ -29,152 +34,158 @@ def write_raw_data(workbook, data, cfg):
 def write_controls_data(workbook, data, cfg):
     ws_controls = workbook.add_worksheet('Kontrollen')    
     ws_controls.set_landscape()
+    ws_controls.set_header('&L&A' + '&CMessergebnisse des Aminosäure-Screenings' + '&RSeite &P von &N')
+    ws_controls.set_footer('&RDatum: &D, &T')
     fmt_heading = workbook.add_format(cfg['format_heading'])
-     
-    splitted_controls = data['selected_control']['data']
-    first = F"1. Wahl: Kontrolle: {str(data['selected_control']['best_control_name'])} Score: {str(data['selected_control']['best_control_score'])}"
-    second = F"2. Wahl: Kontrolle: {str(data['selected_control']['second_best_control_name'])} Score: {str(data['selected_control']['second_best_control_score'])}"
-    ws_controls.write(0, 0, first, fmt_heading)
-    ws_controls.write(1, 0, second, fmt_heading)
+    fmt_heading_orient = workbook.add_format(cfg['format_heading'])
+    fmt_heading_orient.set_rotation(90)
+    fmt_heading_orient.set_align('center')
+    fmt_center = workbook.add_format({'align': 'center'})
+    fmt_even_row = workbook.add_format({'bg_color': '#eeeeee'})
+    fmt_invalid = workbook.add_format(cfg['format_number_invalid'])
     
-    last_idx = 1
-    for key in splitted_controls:
-        ws_controls.write(last_idx+2, 0, "Rohdaten für Kontrolle: " + str(key), fmt_heading)
-        last_idx = write_maxtrix(last_idx+3, 0, splitted_controls[str(key)]['data'], ws_controls, format_header=fmt_heading)
+    ws_controls.write(0, 0, F"1. Wahl: Kontrolle {data['selected_control']}", fmt_heading)
+    
+    row_idx = 2
+    ws_controls.write(row_idx, 0, 'Ranking', fmt_heading_orient)
+    ws_controls.write(row_idx, 1, 'Kontrolle', fmt_heading_orient)
+    ws_controls.write(row_idx, 2, 'Gültige AS', fmt_heading_orient)
+    ws_controls.set_column('D:D', None, None, {'hidden': True})
+    ws_controls.write(row_idx, 3, 'Score', fmt_heading_orient)
+    ws_controls.write_row(row_idx, 5, data['controls'].columns.values.tolist()[2:], fmt_heading_orient)
+    ws_controls.set_column('A:Y', 4.2)
+    ws_controls.set_column('C:C', 4.8)
+    ws_controls.set_column('E:E', 1.5)
+    
+    row_idx = 3
+    first_invalid = None
+    for rank, dat in enumerate(data['checked_controls']):
+        ws_controls.write(row_idx, 0, rank+1, fmt_center)
+        ws_controls.write(row_idx, 1, dat['name'], fmt_heading)
+        ws_controls.write(row_idx, 2, f"{dat['coarse_score']}/20")
+        ws_controls.write(row_idx, 3, f"{round(dat['fine_score']*100,1)}%", fmt_center)
+        if rank%2 == 0:
+            ws_controls.write_row(row_idx, 5, dat['raw_data'].to_numpy()[2:])
+        else:
+            ws_controls.write_row(row_idx, 5, dat['raw_data'].to_numpy()[2:], fmt_even_row)
         
-        ws_controls.write(last_idx+1, 0, "Bereichsanalyse", fmt_heading)
-        last_idx = write_maxtrix(last_idx+2, 0, splitted_controls[str(key)]['checked'], ws_controls, format_header=fmt_heading)
+        res = dat['result']
+        # mark invalids 
+        invalids = res[res=='TOO_LOW'].index.to_list() + res[res=='TOO_HIGH'].index.to_list()
+        invalids = set(invalids)
+        for inv in invalids:
+            pos = res.index.get_loc(inv)
+            ws_controls.conditional_format(row_idx, pos+5, row_idx, pos+5, {'type': 'no_errors', 'format': fmt_invalid}) 
+            # mark heading as invalid
+            if rank == 0:
+                ws_controls.conditional_format(2, pos+5, 2, pos+5, {'type': 'no_errors', 'format': fmt_invalid}) 
         
-        ws_controls.write(last_idx+1, 0, "Wie viele sind im Normbereich?", fmt_heading)
-        last_idx = write_maxtrix(last_idx+2, 0, splitted_controls[str(key)]['score'], ws_controls, format_header=fmt_heading)
+        row_idx += 1
+    
         
-        ws_controls.write(last_idx+1, 0, "Priorisierung der AS", fmt_heading)
-        prios = splitted_controls[str(key)]['prios']
-        greater_zero = prios > 0
-        prios[greater_zero] = 'okay'
-        prios.replace(0, 'unpassend', inplace=True)
-        prios = prios.fillna('ignoriert')
-        last_idx = write_maxtrix(last_idx+2, 0, prios, ws_controls, format_header=fmt_heading)
-
 def write_patients_data(workbook, data, cfg):    
     ws_patients = workbook.add_worksheet('Patienten')
     ws_patients.set_landscape()
-    ws_patients.set_column("A:C", 4.5)
-    ws_patients.set_column("I:I", 4.5)
+    ws_patients.set_header('&L&A' + '&CMessergebnisse des Aminosäure-Screenings' + '&RSeite &P von &N')
+    ws_patients.set_footer('&RDatum: &D, &T')
+    ws_patients.set_column("A:C", 5.2)
+    ws_patients.set_column("I:I", 3.0)
     
-    header3 = '&L&A' + '&CMessergebnisse des Aminosäure-Screenings' + '&RLSeite &P von &N'
-    footer3 = '&RDatum: &D, &T'
-    ws_patients.set_header(header3)
-    ws_patients.set_footer(footer3)
-
     fmt_heading = workbook.add_format(cfg['format_heading'])
+    fmt_heading.set_align('center')
     fmt_heading_right = workbook.add_format(cfg['format_heading'])
     fmt_heading_right.set_align("right")
-    # write additional infos to the the patient sheet
-    #ws_patients.write(0,0,"Messergebnisse des Aminosäure-Screenings")
-    ws_patients.write(0,0,"Normbereich", fmt_heading)
-    ws_patients.write(1,0,"min", fmt_heading_right)
-    ws_patients.write(1,1,"max", fmt_heading_right)
-	    
+    fmt_heading_left = workbook.add_format(cfg['format_heading'])
+    fmt_heading_left.set_align("left")
+    fmt_normal = workbook.add_format(cfg['format_number_valid'])
+    fmt_invalid = workbook.add_format(cfg['format_number_invalid'])
+
     #format the patient data
-    patients = data['data_filtered']
+    patients = data['data']
     pat_ref = data['patients_reference']
     fmt = pd.DataFrame().reindex_like(patients)
+    fmt.iloc[:] = 0  # init with zeros
+    fmt = fmt.astype('int')  # convert to integer
     
+    # iterate by aminos
     for col in pat_ref.columns.values:
-        val_min = pat_ref.loc[0,col]
-        val_max = pat_ref.loc[1,col]
-        col_name = patients.columns[patients.columns.str.contains(pat = col)][0]
-        fmt[col_name][patients[col_name] < val_min] = 1 # mark as too low
-        fmt[col_name][patients[col_name] > val_max] = 2 # mark as too high
-        
-    fmt[patients.columns[data['idx_invalids']]] = -1 # mark as AS invalid
-    fmt.fillna(0, inplace=True) # mark rest as zero = valid
-     
+        val_min, val_max = pat_ref.loc[:, col]
+        fmt[col][patients[col] < val_min] = 1 # mark as too low
+        fmt[col][patients[col] > val_max] = 2 # mark as too high
+    # mark aminos as invalid 
+    res = data['checked_controls'][0]['result']
+    invalids = res[res=='TOO_LOW'].index.to_list() + res[res=='TOO_HIGH'].index.to_list()
+    invalids = list(set(invalids))
+    fmt[invalids] = -1
+
+    # 
     gap_rows = 30
     offset_col = 4
     idx_row = 0
     idx_col = offset_col
     second_part = 0
     fmt_bold = workbook.add_format(cfg['format_heading'])
-    #page_break = []
-    # write min max and amino names 
+    
+    # reformat array with data. Insert empty column
     amino_names = []
     amino_min = []
     amino_max = []
     add_empty_row = [5, 8, 11, 14, 17, 20]
     for idx in range(2, 22):                             
         if idx in add_empty_row:
-            amino_names.append("")
-            amino_min.append("")
-            amino_max.append("")
-        as_name = data['data_filtered'].columns[idx]
+            amino_names.append('')
+            amino_min.append('')
+            amino_max.append('')
+        as_name = data['data'].columns[idx]
         amino_names.append(as_name)
-        amino_min.append(data['patients_reference'].loc[1, as_name[:3]])
-        amino_max.append(data['patients_reference'].loc[0, as_name[:3]])
+        amino_min.append(data['patients_reference'].loc[1, as_name])
+        amino_max.append(data['patients_reference'].loc[0, as_name])
         
-    ws_patients.write_column(idx_row+2, idx_col-1, amino_names, fmt_heading_right)
-    ws_patients.write_column(idx_row+2, idx_col+9, amino_names, fmt_bold)
-    ws_patients.write_column(idx_row+2, idx_col-3, amino_min)
-    ws_patients.write_column(idx_row+2, idx_col-4, amino_max)
-    
-    for (idx, row) in data['data_filtered'].iterrows():
+    # first four patients are printed, an empty columns follows and the next 4 patients are printed.
+    for (idx, patient) in data['data'].iterrows():
+        if idx%8 == 0:
+            idx_row = idx//8*gap_rows
+            # write min max and amino names
+            ws_patients.write(idx_row, 0, "Normbereich", fmt_heading_left)
+            ws_patients.write(idx_row+1, 0, "min", fmt_heading)
+            ws_patients.write(idx_row+1, 1, "max", fmt_heading)
+            ws_patients.write_column(idx_row+2, 0, amino_min, fmt_normal)
+            ws_patients.write_column(idx_row+2, 1, amino_max, fmt_normal)
+            ws_patients.write_column(idx_row+2, 3, amino_names, fmt_heading_right)
+            ws_patients.write_column(idx_row+2, 13, amino_names, fmt_heading_left)
+            for inv in invalids:
+                pos = res.index.get_loc(inv)
+                pos += pos//3
+                ws_patients.conditional_format(idx_row+2+pos, 13, idx_row+2+pos, 13, {'type': 'no_errors', 'format': fmt_invalid})
+                ws_patients.conditional_format(idx_row+2+pos, 3, idx_row+2+pos, 3, {'type': 'no_errors', 'format': fmt_invalid})
+            
+            # add page break
+            offset = gap_rows-1
+            ws_patients.set_row(offset + (idx//8)*gap_rows - 1, 45) 
+            ws_patients.set_row(offset + (idx//8)*gap_rows, 45)
+            
+        if idx%4 == 0 and idx != 0:
+            second_part = 1
+        # after 8 patients a new page shall start
+        if idx%8 == 0 and idx != 0:
+            #idx_row += gap_rows
+            idx_col = offset_col
+            second_part = 0
+        
         # write patient id
-        help_write(cfg, workbook, ws_patients, idx_row, idx_col+second_part, row.iloc[1], 3)    
+        help_write(cfg, workbook, ws_patients, idx_row, idx_col+second_part, int(patient.iloc[1]), 3)   
+
         # write aminos for one patient
         empty_row = 0
         for idx_as in range(2, 22):                             
             if idx_as in add_empty_row:
                 empty_row += 1
-			val = F"{row.iloc[idx_as]:.1f}"
+            val = patient.iloc[idx_as]
             help_write(cfg, workbook, ws_patients, idx_row+idx_as+empty_row, idx_col+second_part, val, fmt.loc[idx][idx_as])
-            
+        
         # go to the next patient slot
         idx_col += 1
-        # first four patients are printed, an empty columns follows
-        if idx_col%(4+offset_col) == 0: 
-            second_part = 1
-        # after 8 patients a new page shall start
-        elif idx_col%(8+offset_col) == 0:
-            idx_col = offset_col # start idx
-            #page_break.append(idx_row)
-            idx_row += gap_rows
-            second_part = 0
-            # write min max and amino names 
-            ws_patients.write(idx_row,0,"Normbereich", fmt_heading)
-            ws_patients.write(idx_row+1,0,"min", fmt_heading_right)
-            ws_patients.write(idx_row+1,1,"max", fmt_heading_right)
-            ws_patients.write_column(idx_row+2, idx_col-1, amino_names, fmt_heading_right )
-            ws_patients.write_column(idx_row+2, idx_col+9, amino_names, fmt_bold )
-            ws_patients.write_column(idx_row+2, idx_col-3, amino_min)
-            ws_patients.write_column(idx_row+2, idx_col-4, amino_max)
     
-    #ws_patients.h_pagebreaks = [20, 40, 80] #page_break
-    #ws_patients.set_row(7, 30) 
-    #ws_patients.set_row(12, 30) 
-    #ws_patients.set_row(17, 30) 
-    #ws_patients.set_row(22, 30) 
-    
-    ws_patients.set_row(28, 45) 
-    ws_patients.set_row(29, 45) 
-    ws_patients.set_row(58, 45) 
-    ws_patients.set_row(59, 45) 
-    ws_patients.set_row(88, 45) 
-    ws_patients.set_row(89, 45) 
-    
-def write_control_data(wb, data, cfg):    
-    ws = wb.add_worksheet('Gewählte Kontrolle')
-    ws.set_landscape()
-    header3 = '&L&A' + '&CMessergebnisse des Aminosäure-Screenings' + '&RSeite &P von &N'
-    footer3 = '&RDatum: &D, &T'
-    ws.set_header(header3)
-    ws.set_footer(footer3)
-    ws.set_column("A:A", 13)
-    ws.set_column("B:Z", 4.5)
-    fmt_heading = wb.add_format(cfg['format_heading'])
-    fmt_heading.set_rotation(90)
-    write_maxtrix(0, 0, data['control_filtered'], ws, format_header=fmt_heading)
-         
 def help_write(cfg, wb, ws, idx_r, idx_c, val, fmt_nr):
     if fmt_nr == -1:
         fmt = wb.add_format(cfg['format_number_invalid'])
@@ -185,7 +196,7 @@ def help_write(cfg, wb, ws, idx_r, idx_c, val, fmt_nr):
     elif fmt_nr == 3:
         fmt = wb.add_format(cfg['format_heading'])
     else:
-        fmt = {}
+        fmt = wb.add_format(cfg['format_number_valid'])
     ws.write(idx_r, idx_c, val, fmt)
     
 def write_maxtrix(idx_row, idx_col, data, worksheet, format_header):
@@ -197,3 +208,21 @@ def write_maxtrix(idx_row, idx_col, data, worksheet, format_header):
         idx_row += 1    
     
     return idx_row
+    
+
+
+def exceltopdf(doc):
+    excel = client.DispatchEx("Excel.Application")
+    excel.Visible = 0
+
+    wb = excel.Workbooks.Open(doc)
+    export_path = os.path.splitext(doc)[0] + '.pdf'
+    wb.WorkSheets([2, 3]).Select()
+    try:
+        #wb.Worksheets[1:].SaveAs(export_path, FileFormat=57)
+        wb.ActiveSheet.ExportAsFixedFormat(0, export_path)
+    except Exception as e:
+        print(f"Failed to convert: {e}")
+    finally:
+        wb.Close()
+        excel.Quit()
